0a871e733477a6305bec6b4d6022bb1434b08f31
tuannguyen <tuannt.ra@gmail.com>
2025-04-20 20:46:36 +0700

    Remove unused file

--- src/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-
-// src/components/layout/index.ts
-export { default as Container } from './Container';
-export { default as Header } from './Header';
--- src/hooks/useTheme.ts
+++
@@ -1,4 +0,0 @@
-import {useTheme as useThemeContext} from '../store/context/ThemeContext';
-
-// Re-export the useTheme hook from the ThemeContext
-export default useThemeContext;
